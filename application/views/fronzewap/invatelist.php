b42c11ee0ea5160c29f1a00d2e7807a143a77cee
whatsns <617035918@qq.com>
2020-03-17 16:04:54 +0800

    更新移动端邀请注册,点击按钮复制邀请信息功能

--- application/views/fronzewap/invatelist.php
+++ application/views/fronzewap/invatelist.php
@@ -29,6 +29,9 @@
   <p>复制邀请注册地址分享给好友:</p>
   <p style="word-break: break-all;">{url user/register/$user['invatecode']}</p>
   {if $this->setting ['credit1_invate']!=null}<p>邀请注册可获得经验值：{$setting['credit1_invate']}点，财富值：{$setting['credit2_invate']}点</p>{/if}
+  
+  <button class="ui-btn copyinvateurl" style="margin-top:10px;margin-bottom:10px" data-clipboard-text="{$user['username']}邀请您加入【{$setting['site_name']}】，点击：{url user/register/$user['invatecode']}" >点击复制分享</button>
+
   </div>
                     <!--{loop $followerlist $index $follower}-->
     <li class="ui-border-t" onclick="window.location.href='{url user/space/$follower['uid']}'">
@@ -52,6 +55,21 @@
           <div class="pages" >{$departstr}</div>   
     </section>
 </section>
+<input type="hidden" id="invatemebtn" value="{url user/register/$user['invatecode']}" />
+<script src="https://cdn.bootcss.com/clipboard.js/2.0.6/clipboard.js"></script>
+<script type="text/javascript">
+var clipboard = new ClipboardJS('.copyinvateurl');
 
+clipboard.on('success', function(e) {
+
+
+    e.clearSelection();
+    alert("复制成功，去粘贴给好友吧")
+});
+
+clipboard.on('error', function(e) {
+    alert("复制失败")
+});
+  </script>
 
 <!--{template footer}-->
