fb4fe0717c8ed1eafa9d33e517d50ebd2afe7a25
whatsns <617035918@qq.com>
2020-05-06 14:05:28 +0800

    优化后台模板管理对模板文件编辑，支持代码高亮编辑

--- application/views/admin/tmp_editfile.php
+++ application/views/admin/tmp_editfile.php
@@ -1,6 +1,7 @@
 <!--{template header}-->
 <SCRIPT src="{SITE_URL}static/js/admin.js" type="text/javascript"></SCRIPT>
 <link href="{SITE_URL}static/css/admin/main.css" rel="stylesheet" type="text/css" />
+
 <div id="append">
 </div>
 <div style="width:100%; height:15px;color:#000;margin:0px 0px 10px;">
@@ -19,20 +20,28 @@
 	</tr>
 </table>
 <!--{/if}-->
-<form action="index.php?admin_template/editdirfile{$setting['seo_suffix']}" method="post">
+<form action="index.php?admin_template/editdirfile/{$dir}/{$dir_file}" onsubmit="return setcode()" method="post">
 <input type="hidden" name="dir" value="{$dir}" />
     <input type="hidden" name="dir_file" value="{$dir_file}" />
+    <input type="hidden" id="tpl_content" name="tpl_content" value="" />
 			<table class="table">
 				<tr class="header">
 					<td colspan="2">模板信息&nbsp;&nbsp;&nbsp;&nbsp;  <input type="button" class="button" name="quxiao" value="返回上一页" onclick="redirect()" ></td>
 				</tr>
 				<tr>
-					<td align="center" style="text-align: center">
+					<td >
 
-                        <textarea class="tpl_textarea" name="tpl_content" style="width: 900px;height: 800px;text-align: center">
-                            {$tpl_content}
-
-                        </textarea>
+                         
+                          <pre>
+                <code class="language-html" id="codefromadmin">
+                {if $_POST}
+                   <div contenteditable="true" style="width:100%;padding:10px;">{eval echo htmlspecialchars($tpl_content);}</div>                                     
+               {else}
+                  <div contenteditable="true" style="width:100%;padding:10px;">{$tpl_content}</div>                             
+               {/if}
+               </code>
+               </pre>
+                          
 					</td>
 				</tr>
 </table>
@@ -43,8 +52,26 @@
             </center><br>
 		</form>
 <br />
-<script>
+   <link rel="stylesheet" type="text/css" href="{SITE_URL}static/js/neweditor/code/styles/tomorrow-night-eighties.css">
+    <script src="{SITE_URL}static/js/neweditor/code/highlight.pack.js" type="text/javascript"></script>
+<script>hljs.initHighlightingOnLoad();</script>
 
+<script>
+    function setcode(){
+        var _html='';
+    	  document.querySelectorAll('pre code').forEach((block) => {
+        	  var _tmptext=$.trim($(block).text());
+        	  if(_tmptext!=''){
+        		  _html=_html+_tmptext;
+        		  console.log(_html);
+        	  }
+    		 
+    		  
+    		 
+      	  });
+    	  $("#tpl_content").val(_html);
+
+    }
     function redirect() {
         var dirfileurl=g_site_url+"index.php?admin_template/default/"+"{$dir}"+"{$setting['seo_suffix']}";
         window.location.href=dirfileurl;
