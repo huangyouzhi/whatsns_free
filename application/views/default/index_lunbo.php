d479cd5a69e989efea745ebc98f5daca138a49cb
whatsns <617035918@qq.com>
2020-08-27 11:53:38 +0800

    更新首页模板

---
+++ application/views/default/index_lunbo.php
@@ -0,0 +1,26 @@
+<div id="slide01" class="tab" onmouseover="this.className='tab tab-active';" onmouseout="this.className='tab';">
+<div class="control" id="slide01_control">
+<a href="javascript:void(0)" class="current"></a>
+<a href="javascript:void(0)" class=""></a>
+<a href="javascript:void(0)" class=""></a>
+</div>
+<a href="javascript:;" class="btnlunbo btnL" onclick="slide01.prev();return false;" onmouseover="slide01.stop();" onmouseout="slide01.play();"></a>
+<a href="javascript:;" class="btnlunbo btnR" onclick="slide01.next();return false;" onmouseover="slide01.stop();" onmouseout="slide01.play();"></a>
+<div class="tabContent">
+
+{eval $lunbolist=array(array('title'=>'喝果汁≠吃水果，宝宝1岁内能不能喝果汁？','image'=>'https://wenda.whatsns.com/data/ueditor/php/upload/image/20200825/1598355780465432.jpg'),array(),array());}
+
+<div class="slideContent"><div class="c" style="display: block;">
+<div class="pic">
+<a target="_blank" href="#" title="喝果汁≠吃水果，宝宝1岁内能不能喝果汁？"><img src="https://wenda.whatsns.com/data/ueditor/php/upload/image/20200825/1598355780465432.jpg" alt="喝果汁≠吃水果，宝宝1岁内能不能喝果汁？" width="660" height="340"><span class="picTit"><em>喝果汁≠吃水果，宝宝1岁内能不能喝果汁？ </em></span></a>
+</div>
+</div><div class="c" style="display: none;">
+<div class="pic">
+<a target="_blank" href="#" title="顺产？还是剖腹产？应该怎么选"><img src="https://wenda.whatsns.com/data/ueditor/php/upload/image/20200825/1598355839147561.jpg" alt="顺产？还是剖腹产？应该怎么选" width="660" height="340"><span class="picTit"><em>顺产？还是剖腹产？应该怎么选 </em></span></a>
+</div>
+</div><div class="c" style="display: none;">
+<div class="pic">
+<a target="_blank" href="#" title="新生儿喂养实操课，开课啦！"><img src="https://wenda.whatsns.com/data/ueditor/php/upload/image/20200825/1598355865139184.jpg" alt="新生儿喂养实操课，开课啦！" width="660" height="340"><span class="picTit"><em>新生儿喂养实操课，开课啦！ </em></span></a>
+</div>
+</div></div></div>
+</div>
